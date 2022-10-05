08c9414eab9e2a901a418bf7c1d416214062b799
Pedro <1758302+pedrococa@users.noreply.github.com>
2022-10-05 11:20:36 +0100

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. ola k ase, provisionas infraestructura en Azure o ke ase?. 
+  Welcome to ${PREFIX}'s app. ola k ase, provisionas infraestructura en Azure con estimación de costes o ke ase?. 
   <!-- END -->
   
   </div>
